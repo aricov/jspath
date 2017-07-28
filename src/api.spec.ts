ecb1e3050729ffd834e1ab1279f56a5584a899ea
Aris Vahlas <aris.vahlas@external.trasysgroup.com>
2017-07-28 20:48:51 +0300

    (feat) api

--- src/api.spec.ts
+++ src/api.spec.ts
@@ -1,6 +1,4 @@
-import * as parser from './parser';
-import { compilePath } from './compiler';
-import * as api from './api';
+import { Path } from './api';
 
 import { expect } from 'chai';
 
@@ -32,40 +30,42 @@ describe('End to end usage', () => {
     };
 
     it ('Should find the second book', () => {
-        const path = compilePath(parser.parse('$.catalog[1]'));
-        expect(api.values(state, path)).to.deep.equal([state.catalog[1]]);
+        const actual = Path.parse('$.catalog[1]')
+                .compile()
+                .value(state);
+        expect(actual).to.deep.equal(state.catalog[1]);
     });
 
     it( 'should extract all the prices', () => {
-         const path = compilePath(parser.parse('$.catalog[*].price'));
+         const actual = Path.compile('$.catalog[*].price').values(state);
          const expected = state.catalog.map(bk => bk.price);
-         expect(api.values(state, path)).to.deep.equal(expected);
+         expect(actual).to.deep.equal(expected);
     });
 
     it( 'should filter selection by quantity', () => {
-         const path = compilePath(parser.parse('$.basket.selection[? .quantity is 2]'));
-         expect(api.values(state, path)).to.deep.equal([state.basket.selection[0]]);
+         const actual = Path.compile('$.basket.selection[? .quantity is 2]')
+                .value(state);
+         expect(actual).to.deep.equal(state.basket.selection[0]);
     });
 
     it('should filter the first selection by quantity', () => {
-        const path = compilePath(parser.parse('$[? @.selection[0].quantity is 2].total'));
-        expect(api.values(state, path)).to.deep.equal([15.98]);
+        const path = Path.compile('$[? @.selection[0].quantity is 2].total');
+        expect(path.value(state)).to.equal(15.98);
     });
 
     it('should filter selection children by quantity', () => {
-        const path = compilePath(parser.parse('$[? 2 in @.selection[*].quantity].total'));
-        expect(api.values(state, path)).to.deep.equal([15.98]);
+        const path = Path.compile('$[? 2 in @.selection[*].quantity].total');
+        expect(path.value(state)).to.equal(15.98);
     });
 
     it('should find basket products with quantity of 2', () => {
-        const path = compilePath(parser.parse('$.basket.selection[? .quantity is 2].product'));
-        expect(api.values(state, path)).to.deep.equal([20]);
+        const path = Path.compile('$.basket.selection[? .quantity is 2].product');
+        expect(path.value(state)).to.equal(20);
     });
  
-    it('should find the catalog prices of the books included in the basket selection', ()=> {
-        const path = compilePath(parser.parse('$.catalog[? .id in $.basket.selection[*].product].price'));
-        expect(path.multi).true;
-        expect(api.values(state, path)).to.deep.equal([ state.catalog[1].price ]);
+    it('should find the catalog prices of the books included in the basket selection', () => {
+        const path = Path.compile('$.catalog[? .id in $.basket.selection[*].product].price');
+        expect(path.value(state)).to.equal( state.catalog[1].price );
     });
 
 });
--- src/api.ts
+++ src/api.ts
@@ -1,8 +1,45 @@
-import { PathMatcher, CompiledExpression } from './compiler';
+import * as ast from './ast';
+import * as parser from './parser';
+import { PathMatcher, CompiledExpression, compilePath, compileExpression } from './compiler';
 
 export { PathMatcher, CompiledExpression };
 
-export const values = (source: any, path: PathMatcher): any[] => {
-    return path.match([source]).map(match => match.value);
+export const Path = {
+
+    parse: (expression: string) => {
+        const ast = parser.parse(expression) as ast.Path;
+        return {
+            ast,
+            compile: () => {
+                const matcher = compilePath(ast);
+                return {
+                    matches: (source: any) => matcher.match([source]),
+                    values: (source: any) => matcher.match([source]).map(m => m.value),
+                    value: (source: any) => matcher.match([source]).reduce((v:any, m) => (v === undefined ? m.value : v), undefined)
+                };
+            }
+        };
+    },
+
+    compile: (expression: string) => Path.parse(expression).compile()
 };
 
+export const Expression = {
+
+    parse: (expression: string) => {
+        const ast = parser.parse(expression, {startRule:'expr'});
+        return {
+            ast,
+            compile: () => {
+                const expr = compileExpression(ast);
+                return {
+                    test: (source: any) => expr([source])
+                };
+            }
+        };
+    },
+
+    compile: (expression: string) => Expression.parse(expression).compile(),
+
+    test: (expression: string, source: any) => Expression.compile(expression).test(source)
+};
